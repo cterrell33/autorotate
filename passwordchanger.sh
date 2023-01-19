813edb052ef5e41a259acb69e098b2e11f98eb3a
cterrell133 <cterrell@kingdomtech.net>
2023-01-19 11:42:06 -0500

    testbuild

--- passwordchanger.sh
+++ passwordchanger.sh
@@ -5,7 +5,7 @@ currentpw=$(aws secretsmanager get-secret-value --secret-id $users --query Secre
 newpw=$(aws secretsmanager get-random-password --require-each-included-type --password-length 12 --output text)
 
 
-echo -e "$currentpw""\n""$newpw""\n""$newpw""\n""$newpw"|smbpasswd -U $users -r 172.31.0.132
+echo -e "$currentpw""\n""$newpw""\n""$newpw""\n""$newpw"|smbpasswd -U test1 -r 172.31.0.132
 if [ $? -eq 0 ]; then
     echo "Password Change Successfully"
 else 
