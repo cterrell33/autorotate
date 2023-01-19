2720589979e923ae1f1e203135cd9d301bdb5630
cterrell133 <cterrell@kingdomtech.net>
2023-01-19 11:51:46 -0500

    testbuild

--- passwordchanger.sh
+++ passwordchanger.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-currentpw=$(aws secretsmanager get-secret-value --secret-id test1 --query SecretString --output text)
-newpw=$(aws secretsmanager get-random-password --require-each-included-type --password-length 12 --output text)
+currentpw=$(aws secretsmanager get-secret-value --secret-id test2 --query SecretString --output text)
+newpw=$(aws secretsmanager get-random-password --require-each-included-type --password-length 14 --output text)
 
 
-echo -e "$currentpw""\n""$newpw""\n""$newpw""\n""$newpw"|smbpasswd -U test1 -r 172.31.0.132
+echo -e "$currentpw""\n""$newpw""\n""$newpw""\n""$newpw"|smbpasswd -U test2 -r 172.31.0.132
 if [ $? -eq 0 ]; then
     echo "Password Change Successfully"
 else 
@@ -13,7 +13,7 @@ else
 fi 
 
 # Update Secrets Manager with new Random Generated PW
-aws secretsmanager update-secret --secret-id test1 --secret-string "$newpw"
+aws secretsmanager update-secret --secret-id test2 --secret-string "$newpw"
 if [ $? -eq 0 ]; then
     echo "New PW Stored Successfully"
 else 
