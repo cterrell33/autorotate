d77b21631e649aa147556d764fbdbf7ec2753027
cterrell133 <cterrell@kingdomtech.net>
2023-01-19 12:42:30 -0500

    testbuild

--- passwordchanger.sh
+++ passwordchanger.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-users=("test1" "test2")
-currentpw=$(aws secretsmanager get-secret-value --secret-id $users --query SecretString --output text)
+# users=$("test1" "test2")
+currentpw=$(aws secretsmanager get-secret-value --secret-id test1 --query SecretString --output text)
 newpw=$(aws secretsmanager get-random-password --require-each-included-type --password-length 14 --output text)
 
 
-for i in "${users[@]};"
-do
-        echo -e "$currentpw""\n""$newpw""\n""$newpw""\n""$newpw"|smbpasswd -U "$i" -r 172.31.0.132
-    if [ $? -eq 0 ]; then
-        echo "Password Change Successfully"
-    else 
-        echo "Password Changed Failed"
-    fi
-    
-done
+echo -e "$currentpw""\n""$newpw""\n""$newpw""\n""$newpw"|smbpasswd -U test1 -r 172.31.0.132
+if [ $? -eq 0 ]; then
+    echo "Password Change Successfully"
+else 
+    echo "Password Changed Failed"
+fi 
 
 # Update Secrets Manager with new Random Generated PW
-for i in "${users[@]};"
-do
-        aws secretsmanager update-secret --secret-id "$i" --secret-string "$newpw"
-        
-    if [ $? -eq 0 ]; then
-        echo "New PW Stored Successfully"
-    else 
-        echo "Password not Stored in Secrets Manager"
-    fi
-done
+aws secretsmanager update-secret --secret-id test1 --secret-string "$newpw"
+if [ $? -eq 0 ]; then
+    echo "New PW Stored Successfully"
+else 
+    echo "Password not Stored in Secrets Manager"
+fi 
